a9b7f7ec69c8b7bd437fc7b9e66de33f93790a09
janily <nanhuchenjie@163.com>
2024-07-28 17:25:52 +0800

    feat: 文本测试

--- index.js
+++ index.js
@@ -1,61 +1,66 @@
-const path = require("path");
 const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
-
-const logger = morgan("tiny");
+const xml2js = require('xml2js');
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
-app.use(logger);
+app.use(express.text({ type: 'text/xml' }));
 
-// 首页
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
+// 解析XML的辅助函数
+function parseXML(xml) {
+  return new Promise((resolve, reject) => {
+    xml2js.parseString(xml, { explicitArray: false }, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result.xml);
+      }
     });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
   });
-});
+}
 
-// 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
-});
-
-// 小程序调用，获取微信 Open ID
-app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
+// 微信消息处理
+app.post('/', async (req, res) => {
+  try {
+    const message = await parseXML(req.body);
+    
+    if (message.MsgType === 'text') {
+      let replyContent = '收到你的消息了';
+      
+      if (message.Content === '你好') {
+        replyContent = '欢迎来到大字报';
+      }
+      
+      const replyMessage = `
+        <xml>
+          <ToUserName><![CDATA[${message.FromUserName}]]></ToUserName>
+          <FromUserName><![CDATA[${message.ToUserName}]]></FromUserName>
+          <CreateTime>${Math.floor(Date.now() / 1000)}</CreateTime>
+          <MsgType><![CDATA[text]]></MsgType>
+          <Content><![CDATA[${replyContent}]]></Content>
+        </xml>
+      `;
+      res.type('application/xml');
+      res.send(replyMessage);
+    } else {
+      const replyMessage = `
+        <xml>
+          <ToUserName><![CDATA[${message.FromUserName}]]></ToUserName>
+          <FromUserName><![CDATA[${message.ToUserName}]]></FromUserName>
+          <CreateTime>${Math.floor(Date.now() / 1000)}</CreateTime>
+          <MsgType><![CDATA[text]]></MsgType>
+          <Content><![CDATA[仅支持文本消息]]></Content>
+        </xml>
+      `;
+      res.type('application/xml');
+      res.send(replyMessage);
+    }
+  } catch (error) {
+    console.error('Error processing message:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
 const port = process.env.PORT || 80;
 
-async function bootstrap() {
-  await initDB();
-  app.listen(port, () => {
-    console.log("启动成功", port);
-  });
-}
-
-bootstrap();
+app.listen(port, () => {
+  console.log("启动成功", port);
+});
